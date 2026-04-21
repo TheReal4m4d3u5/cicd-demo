5408f22f172dfc8536e1fc0835c3bdca9f8899d8
TheReal4m4d3u5 <averyajacobson@gmail.com>
2026-04-21 08:15:51 -0500

    Break test intentionally

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello CI/CD' });
+  res.json({ message: 'Broken CI' });
 });
 
 module.exports = app;
